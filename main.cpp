548a422e533857ee33dfd486930be8258156b161
Hunt Ma <huntma@users.noreply.github.com>
2016-12-02 02:32:38 -0800

    Not working IR
    Uses IR when not approaching corner. Uses encoders otherwise.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
+
 #include "mbed.h"
 
 DigitalOut led1(LED1);
 
+PwmOut rightF(PA_7);
+PwmOut rightR(PB_6);
 
-PwmOut rightR(PA_7);
-PwmOut rightF(PB_6);
-
-PwmOut leftF(PC_7);
-PwmOut leftR(PB_10);
+PwmOut leftR(PC_7);
+PwmOut leftF(PB_10);
 
 
 InterruptIn encoderRightR(PB_3);
 InterruptIn encoderRightF(PA_15);
 InterruptIn encoderLeftR(PA_1);
 InterruptIn encoderLeftF(PC_4);
+
+//IR receivers and corresponding pins (directions relative to forward-facing rat)
+AnalogIn ir_r1(PC_0);       //far-left
+AnalogIn ir_r2(PC_1);       //Middle-left
+AnalogIn ir_r3(PA_4);       //Middle-right
+AnalogIn ir_r4(PA_0);       //far-right
+//IR emitters and corresponding pins
+DigitalOut ir_e1(PB_7);     //far-left
+DigitalOut ir_e2(PB_0);     //Middle-left
+DigitalOut ir_e3(PC_11);    //Middle-right
+DigitalOut ir_e4(PC_10);    //far-right
 
 Timer timer;
 
@@ -26,12 +37,20 @@ double Ki = 0.5;
 double speedChange = 0;     //holds P,I,D fn return values
 int integral = 0;
 double maxCorrect = 0.1;    //limits correction values
-double maxSpeed = 0.5;     //limits motor speed
+double maxSpeed = 0.3;     //limits motor speed
 double minSpeed = 0.05;     //need this?
 double speedR = 0.1;        //for setting motor speed; accumulates
 double speedL = 0.1;
 int cnt=0;                  //for D
 int prevError = 0;  //for D
+
+
+    float baseMidRight = 0;
+    float baseMidLeft = 0;
+    float baseFarRight = 0;
+    float baseFarLeft = 0;
+
+
 
 Serial pc(PA_9, PA_10); //set serial
 
@@ -49,17 +68,17 @@ void stop() {    // stops mouse (turns motors to 0)
     rightR = 0;
     leftF = 0;
     leftR = 0;
-    wait(0.3);
+    wait(0.1);
     return;
 }
 
-void turnL90() {   // turns 90 deg right
+void turnR90() {   // turns 90 deg right
     int temp = pulsesRight;
     int temp2 = pulsesLeft;
     pulsesRight = 0;
     pulsesLeft = 0;
     //int n = 2500*cells;
-    while(pulsesRight<2000){
+    while(pulsesRight<300){
         rightF = 0.1;
         rightR = 0;
         leftF = 0;
@@ -71,13 +90,13 @@ void turnL90() {   // turns 90 deg right
     return;
 }
 
-void turnR90() {   // turns 90 deg right
+void turnL90() {   // turns 90 deg right
     int temp = pulsesRight;
     int temp2 = pulsesLeft;
     pulsesRight = 0;
     pulsesLeft = 0;
     //int n = 2500*cells;
-    while(pulsesRight<2000){
+    while(pulsesLeft<300){
         rightF = 0;
         rightR = 0.1;
         leftF = 0.1;
@@ -89,6 +108,8 @@ void turnR90() {   // turns 90 deg right
     return;
 }
 
+/*
+//Not in use
 void forward(int cells) {   // moves forward num of cells
     int temp = pulsesRight;
     pulsesRight = 0;
@@ -96,13 +117,13 @@ void forward(int cells) {   // moves forward num of cells
     while(pulsesRight<n){
         rightF = 0.1;
         rightR = 0;
-        leftF = 0;
-        leftR = 0.1;
+        leftF = 0.1;
+        leftR = 0;
     }
     pulsesRight = temp + pulsesRight;
     stop();
     return;
-}
+}*/
 
 int speedLeft(double speed){    // writes the speed of left motor (checks if valid)
     if (speed < maxSpeed){
@@ -111,6 +132,7 @@ int speedLeft(double speed){    // writes the speed of left motor (checks if val
     }
     return 0;
 }
+
 int speedRight(double speed){    // writes speed of right motor (checks if valid)
     if (speed < maxSpeed){
         rightF.write(speed);
@@ -164,54 +186,124 @@ double I_Controller(int error)
 }
 
 
+void aheadTest(float midRight, float midLeft, float farRight, float farLeft)
+{
+    if(midRight >= 0.825 || midLeft >= 0.825){
+        pc.printf("hi\r\n");
+        stop();
+
+        if (farRight > farLeft) {
+            pc.printf("turnLeft\r\n");
+            turnL90();
+        }
+        else {
+            pc.printf("turnRight\r\n");
+            turnR90();
+        }
+    } 
+
+    return;
+}
+
 int main() {
     
+	wait(1);
+	
     encoderRightF.rise(&incrementRight);
     encoderRightF.fall(&incrementRight);
     encoderLeftF.rise(&incrementLeft);
     encoderLeftF.fall(&incrementLeft);
     pc.baud(9600);
 
-    turnR90();
-    /*
+    int i = 0;
+
+    while(i < 100){	//if trying to avg 100, need +=
+        ir_e4 = 1;
+        ir_e3 = 1;
+        ir_e2 = 1;
+        ir_e1 = 1;
+        baseMidRight += ir_r3.read();
+        baseMidLeft += ir_r2.read();
+        baseFarRight += ir_r4.read();
+        baseFarLeft += ir_r1.read();
+        i++;
+    }
+
+    baseMidLeft /= 100;
+    baseMidRight /= 100;
+    baseFarRight /= 100;
+    baseFarLeft /= 100;
+	
+
     while(1) {
+
         timer.start();
         led1 = 1;
-        errorPulse = pulsesRight - pulsesLeft; //if errorPulse negative: left is faster than right
-
-
-
-        //PROPORTIONAL
-        speedChange = P_Controller(errorPulse); //can be neg or pos
         
-        //DERIVATIVE
-        speedChange += D_Controller(errorPulse); //when P neg, D is pos; this line not tested
+        ir_e4 = 1;
+        ir_e3 = 1;
+        ir_e2 = 1;
+        ir_e1 = 1;
 
-        //INTEGRAL
-        //speedChange += I_Controller(errorPulse);
-      
-        speedR -= speedChange; //if speedChange neg (left is faster), speedR increases
-        speedL += speedChange;
+        float midRight = ir_r3.read();
+        float midLeft = ir_r2.read();
+        float farRight = ir_r4.read();
+        float farLeft = ir_r1.read();
 
-         if (errorPulse == 0) { //if no error go normal speed
-            speedR = 0.4;
-            speedL = 0.4;
-        }
+        aheadTest(midRight, midLeft, farRight, farLeft);
+        errorPulse = pulsesLeft - pulsesRight; //if errorPulse negative: left is faster than right
+
+        //when front sensors go over 0.2, ignore sides
+
+        //--------IR PID-----------
+
+        float changeMidRight = midRight - baseMidRight;
+        float changeMidLeft = midLeft - baseMidLeft;
+        float changeFarRight = farRight - baseFarRight;
+        float changeFarLeft = farLeft - baseFarLeft;
+
+        int enc_pid = 1;	//toggles encoder or IR PID
+		
+		if((changeMidRight + changeMidLeft)*0.5 > 0.1) {	//only use IR when not approach corner
+			if(changeFarRight > 0.1){ //approaching right wall
+				speedR += 0.1;
+				pulsesRight=0;
+				pulsesLeft=0;
+				enc_pid=0;
+			}else if(changeFarLeft > 0.1){
+				speedL += 0.1;
+				pulsesRight=0;
+				pulsesLeft=0;
+				enc_pid=0;
+			}
+
+		}
+
+        if(enc_pid) {
+			//PROPORTIONAL
+			speedChange = P_Controller(errorPulse); //can be neg or pos
+			
+			//DERIVATIVE
+			speedChange += D_Controller(errorPulse); //when P neg, D is pos; this line not tested
+
+			//INTEGRAL
+			//speedChange += I_Controller(errorPulse);
+		  
+			speedR -= speedChange; //if speedChange neg (left is faster), speedR increases
+			speedL += speedChange;
+
+			 if (errorPulse == 0) { //if no error go normal speed
+				speedR = 0.2;
+				speedL = 0.2;
+			}
+		}
                
         speedLeft(speedL);
         speedRight(speedR);
         
-        //pc.printf("%d\r", integral);
-        //pc.printf(" ");
-        //pc.printf("%f\r", speedChange);
-        //pc.printf(" ");
         pc.printf("%d\r\n", errorPulse);
 
-         //pc.printf("error: %d\n", errorPulse);
-       
         timer.stop();
-
-
-    } */
+        
+    }
 }
-
