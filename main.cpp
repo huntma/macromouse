1c523de912d69c8785938e4d1784ac92b4e5ed71
Hunt Ma <reachhunt@gmail.com>
2017-05-24 18:42:31 -0700

    remove unnecessary ir reads and clean up speed and systick fns

--- main.cpp
+++ main.cpp
@@ -41,263 +41,229 @@ double Ki = 0.5;
 double speedChange = 0;     //holds P,I,D fn return values
 int integral = 0;
 double maxCorrect = 0.1;    //limits correction values
-double maxSpeed = 0.5;     //limits motor speed
-double minSpeed = 0.1;     //need this?
+double maxSpeed = 0.2;     //limits motor speed
+double minSpeed = 0.05;     //need this?
 double speedR = 0.1;        //for setting motor speed; accumulates
 double speedL = 0.1;
 int cnt=0;                  //for D
 int prevError = 0;  //for D
 int turning = 0;
 
-float farLeft =  ir_r1.read();
-float midLeft =  ir_r2.read();
-float midRight = ir_r3.read();
-float farRight = ir_r4.read();
+float farLeft;
+float midLeft;
+float midRight;
+float farRight;
 
 void incrementRight(){  // increments right pulses
-    pulsesRight++;
-    return;
+	pulsesRight++;
+	return;
 }
 void incrementLeft(){  //increments left pulses
-    pulsesLeft++;
-    return;
+	pulsesLeft++;
+	return;
 }
 
 void stop() {    // stops mouse (turns motors to 0)
-    rightF = 0;
-    rightR = 0;
-    leftF = 0;
-    leftR = 0;
-    wait(0.05);
-    farRight = ir_r4.read();
-    farLeft = ir_r1.read();
-    return;
+	rightF = 0;
+	rightR = 0;
+	leftF = 0;
+	leftR = 0;
+	return;
+}
+
+void go(int cells) {
+	setRight(speedR)
 }
 
 void turnR90() {   // turns 90 deg right
-    turning = 1;
-    int temp = pulsesRight;
-    int temp2 = pulsesLeft;
-    pulsesRight = 0;
-    pulsesLeft = 0;
-    
-    midRight = ir_r3.read();
-    while(pulsesLeft<1000 && midRight > 0.35){
-        pc.printf("pulsesLeft:%d              midRight:%f\r\n", pulsesLeft, midRight);
-        rightF.write(0.1);
-        rightR.write(0);
-        leftF.write(0);
-        leftR.write(0.1);
-        midRight = ir_r3.read();
-    }
-    pulsesRight = temp;
-    pulsesLeft = temp2;
-    stop();
-    turning = 0;
-    return;
+	turning = 1;
+	int temp = pulsesRight;
+	int temp2 = pulsesLeft;
+	pulsesRight = 0;
+	pulsesLeft = 0;
+	
+	midRight = ir_r3.read();
+	while(pulsesLeft<1000 && midRight > 0.35){
+		pc.printf("pulsesLeft:%d              midRight:%f\r\n", pulsesLeft, midRight);
+		rightF.write(0.1);
+		rightR.write(0);
+		leftF.write(0);
+		leftR.write(0.1);
+		midRight = ir_r3.read();
+	}
+	pulsesRight = temp;
+	pulsesLeft = temp2;
+	stop();
+	turning = 0;
+	return;
 }
 
 void turnL90() {   // turns 90 deg right
-    turning = 1;
-    int temp = pulsesRight;
-    int temp2 = pulsesLeft;
-    pulsesRight = 0;
-    pulsesLeft = 0;
-    midLeft = ir_r2.read();
-    while(pulsesRight<1000 && midLeft > 0.35){
-        pc.printf("pulsesRight:%d\r\n", pulsesRight);
-        rightF.write(0);
-        rightR.write(0.1);
-        leftF.write(0.1);
-        leftR.write(0);
-        midLeft = ir_r2.read();
-    }
-    pulsesRight = temp;
-    pulsesLeft = temp2;
-    stop();
-    turning = 0;
-    return;
+	turning = 1;
+	int temp = pulsesRight;
+	int temp2 = pulsesLeft;
+	pulsesRight = 0;
+	pulsesLeft = 0;
+	midLeft = ir_r2.read();
+	while(pulsesRight<1000 && midLeft > 0.35){
+		pc.printf("pulsesRight:%d\r\n", pulsesRight);
+		rightF.write(0);
+		rightR.write(0.1);
+		leftF.write(0.1);
+		leftR.write(0);
+		midLeft = ir_r2.read();
+	}
+	pulsesRight = temp;
+	pulsesLeft = temp2;
+	stop();
+	turning = 0;
+	return;
 }
 
 /*
 //Not in use yet
 void forward(int cells) {   // moves forward num of cells
-    int temp = pulsesRight;
-    pulsesRight = 0;
-    int n = 2500*cells;
-    while(pulsesRight<n){
-        rightF = 0.1;
-        rightR = 0;
-        leftF = 0.1;
-        leftR = 0;
-    }
-    pulsesRight = temp + pulsesRight;
-    stop();
-    return;
+	int temp = pulsesRight;
+	pulsesRight = 0;
+	int n = 2500*cells;
+	while(pulsesRight<n){
+		rightF = 0.1;
+		rightR = 0;
+		leftF = 0.1;
+		leftR = 0;
+	}
+	pulsesRight = temp + pulsesRight;
+	stop();
+	return;
 }*/
 
-void speedLeft(double speed){    // writes the speed of left motor (checks if valid)
-    if ((speed < maxSpeed) && (speed > minSpeed)){
-        leftF.write(speed);
-        return;
-    }else if(speed > maxSpeed){
-        leftF.write(maxSpeed);
-        return;
-    }else if(speed < minSpeed){
-        leftF.write(minSpeed);
-        return;
-    }else{
-        leftF.write(minSpeed);
-    }
-    return;
+void setLeft(double speed){    // safely writes motor speed
+	if(speed > maxSpeed)
+		leftF.write(maxSpeed);
+	else if(speed < minSpeed)
+		leftF.write(minSpeed);
+	else
+		leftF.write(speed);
+	return;
 }
 
-void speedRight(double speed){    // writes speed of right motor (checks if valid)
-    if ((speed < maxSpeed) && (speed > minSpeed)){
-        rightF.write(speed);
-        return;
-    }else if(speed > maxSpeed){
-        rightF.write(maxSpeed);
-        return;
-    }else if(speed < minSpeed){
-        rightF.write(minSpeed);
-        return;
-    }else{
-        rightF.write(minSpeed);
-    }
-    return;
+void setRight(double speed){
+	if(speed > maxSpeed)
+		rightF.write(maxSpeed);
+	else if(speed < minSpeed)
+		rightF.write(minSpeed);
+	else
+		rightF.write(speed);
+	return;
 }
 
 void resetSpeed(double speed){    // resets forward speeds to input
-    rightF.write(speed);
-    rightR.write(0);
-    leftF.write(speed);
-    leftR.write(0);
-    return;
+	rightF.write(speed);
+	rightR.write(0);
+	leftF.write(speed);
+	leftR.write(0);
+	return;
 }
 
 double P_Controller(int error) // multiplies by Kp
 {
-    double correction = Kp * error;  // check if this is the right constant (1 pulse = 1 rotation of inner shaft, = ____ wheel rotations)
-        if (abs(correction) > maxCorrect) // check with max
-            correction = maxCorrect;
-    return correction;
+	double correction = Kp * error;  // check if this is the right constant (1 pulse = 1 rotation of inner shaft, = ____ wheel rotations)
+		if (abs(correction) > maxCorrect) // check with max
+			correction = maxCorrect;
+	return correction;
 }
 
 double D_Controller(int error) //calc D correction
 {
-    if(cnt==0) {    //once per 10 cycles
-        int dError = error - prevError; //fairly small #
-        double correction = Kd * dError;
-        prevError = error;
-        if (abs(correction) > maxCorrect) // check with max
-            correction = maxCorrect;     
-        return correction;
-    }
-    cnt++;
-    cnt = cnt%10;
-    return 0;
+	if(cnt==0) {    //once per 10 cycles
+		int dError = error - prevError; //fairly small #
+		double correction = Kd * dError;
+		prevError = error;
+		if (abs(correction) > maxCorrect) // check with max
+			correction = maxCorrect;     
+		return correction;
+	}
+	cnt++;
+	cnt = cnt%10;
+	return 0;
 }
 
 double I_Controller(int error)
 {
-    integral += error;
-    double correction = Ki * integral;
-    integral /= 2; //decay factor is 2
-    return correction;
+	integral += error;
+	double correction = Ki * integral;
+	integral /= 2; //decay factor is 2
+	return correction;
 }
 
+//needs changes. maybe into systick, but not checking walls and turning should be main loop
 void aheadTest()
 {
-    pc.printf("test farLeft: %f         farRight:%f\r\n", farLeft, farRight);
-    midRight = ir_r3.read();
-    midLeft = ir_r2.read();
-    if(midRight >= 0.325 || midLeft >= 0.325){
-        farRight = ir_r4.read();
-        farLeft = ir_r1.read();
-        if (farRight > farLeft+0.1) {
-            turnL90();
-        }
-        else if(0.1+farLeft > farRight){
-            turnR90();
-        }
-    } 
-    return;
+	//pc.printf("test farLeft: %f         farRight:%f\r\n", farLeft, farRight);
+	midRight = ir_r3.read();
+	midLeft = ir_r2.read();
+	if(midRight >= 0.325 || midLeft >= 0.325){
+		farRight = ir_r4.read();
+		farLeft = ir_r1.read();
+		if (farRight > farLeft+0.1) {
+			turnL90();
+		}
+		else if(0.1+farLeft > farRight){
+			turnR90();
+		}
+	} 
+	return;
 }
 
 void systick() {
-    
-    //pc.printf("%f\r %f\r %f\r %f\r\n", farLeft, midLeft, midRight, farLeft);
+	
+	//errorPulse = pulsesLeft - pulsesRight;
 
-    errorPulse = pulsesLeft - pulsesRight; //if errorPulse negative: left is faster than right
+	//--------IR PID----------- just avoid walls
+	
+	//systick will run while turning so turn off ir pid while turning.
+	if(!turning) {
+		if(midRight > 0.1) //approaching right wall
+			speedR += 0.3 * midRight;   //avg midRight a couple cm away is 0.2
+		else if(farLeft > 0.1)
+			speedL += 0.3 * midLeft;
+	}
 
-    //--------IR PID-----------
-    
-    midRight = ir_r3.read();
-    midLeft = ir_r2.read();
-    farRight = ir_r4.read();
-    farLeft = ir_r1.read();
-    
-    if(!turning) {
-    
-    if(farLeft > 0.6 || farRight > 0.6) {  //only use IR when not approach corner
-        if(farRight > 0.6){ //approaching right wall
-            speedChange -= 0.1 * midRight;  //need to add proportional
-//          pulsesRight=0;
-//          pulsesLeft=0;
-        }else if(farLeft > 0.6){
-            speedChange += 0.1 * midLeft;
-//          pulsesRight=0;
-//          pulsesLeft=0;
-        }   
-        speedR -= speedChange;
-        speedL += speedChange;
-    }
+	//encoder PID , may not need
+	/*
+	else{
+		//PROPORTIONAL
+		speedChange = P_Controller(errorPulse); //can be neg or pos
+		
+		//DERIVATIVE
+		speedChange += D_Controller(errorPulse); //when P neg, D is pos; this line not tested
 
-    else{
-        //PROPORTIONAL
-        speedChange = P_Controller(errorPulse); //can be neg or pos
-        
-        //DERIVATIVE
-        speedChange += D_Controller(errorPulse); //when P neg, D is pos; this line not tested
+		//INTEGRAL
+		//speedChange += I_Controller(errorPulse);
+	  
+		speedR += speedChange; //if speedChange neg (left is faster), speedR increases
+		speedL -= speedChange;
 
-        //INTEGRAL
-        //speedChange += I_Controller(errorPulse);
-      
-        speedR += speedChange; //if speedChange neg (left is faster), speedR increases
-        speedL -= speedChange;
-
-        if (errorPulse == 0) { //if no error go normal speed
-            speedR = 0.2;
-            speedL = 0.2;
-        }
-    }
-           
-    speedLeft(speedL);
-    speedRight(speedR);
-    
-    //pc.printf("%f\r %f\r\n", speedL, speedR);
-    }
-    
-    speedL = 0;
-    speedR = 0;
-
+		if (errorPulse == 0) { //if no error go normal speed
+			speedR = 0.2;
+			speedL = 0.2;
+		}
+	}*/
 }
 
 int main() {
-	
+	pc.baud(9600);
 	onled = 1;
-    encoderRightF.rise(&incrementRight);
-    encoderRightF.fall(&incrementRight);
-    encoderLeftF.rise(&incrementLeft);
-    encoderLeftF.fall(&incrementLeft);
+	encoderRightF.rise(&incrementRight);
+	encoderRightF.fall(&incrementRight);
+	encoderLeftF.rise(&incrementLeft);
+	encoderLeftF.fall(&incrementLeft);
+	//not touching R pins of motors yet
 
-    pc.baud(9600);
-
-		float offsetL;
-		float offsetML;
-		float offsetMR;
-		float offsetR;
+	float offsetL;
+	float offsetML;
+	float offsetMR;
+	float offsetR;
 
 	for(int i=0; i<9; i++) {
 		ir_e1 = 1;
@@ -313,37 +279,53 @@ int main() {
 		offsetR  += ir_r4.read();
 		ir_e4 = 0;
 	}
-		offsetL  /= 10;
-		offsetML /= 10;
-		offsetMR /= 10;
-		offsetR  /= 10;
-        
-    leftF.write(0.0);
-    leftR.write(0);
+	offsetL  /= 10;
+	offsetML /= 10;
+	offsetMR /= 10;
+	offsetR  /= 10;
+	
+	leftF.write(0);
+	leftR.write(0);
 
-    rightF.write(0.0);
-    rightR.write(0);
-    
-    while (1) {
-			ir_e1 = 1;
-			farLeft = ir_r1.read() - offsetL;
-			ir_e1 = 0;
-			
-			ir_e2 = 1;
-			midLeft = ir_r2.read() - offsetML;
-			ir_e2 = 0;
-			
-			ir_e3 = 1;
-			midRight = ir_r3.read() - offsetMR;
-			ir_e3 = 0;
+	rightF.write(0);
+	rightR.write(0);
+	
+	while (1) {
+		ir_e1 = 1;
+		farLeft = ir_r1.read() - offsetL;
+		ir_e1 = 0;
+		
+		ir_e2 = 1;
+		midLeft = ir_r2.read() - offsetML;
+		ir_e2 = 0;
+		
+		ir_e3 = 1;
+		midRight = ir_r3.read() - offsetMR;
+		ir_e3 = 0;
 
-			ir_e4 = 1;
-			farRight = ir_r4.read() - offsetR;
-			ir_e4 = 0;
+		ir_e4 = 1;
+		farRight = ir_r4.read() - offsetR;
+		ir_e4 = 0;
+					
+	if(!turning) {
+		if(midRight > 0.1) { //approaching right wall
+			speedR += 0.3 * midRight;
+			speedL -= 0.1 * midRight;
+			//setRight(speedR);
+			//pc.printf("speedR : %f\n", speedR);
+			speedR = 0; speedL = 0;
+		} else if(farLeft > 0.1) {
+			speedL += 0.3 * midLeft;
+			speedR -= 0.1 * midLeft;
+			//setLeft(speedL);
+			//pc.printf("speedL: %f\n", speedL);
+			speedL = 0; speedR = 0;
+		}
+	}
 
-			pc.printf("%f %f %f %f \n", farLeft, midLeft, midRight, farRight);
+	//pc.printf("%f %f %f %f %f %f\n", farLeft, midLeft, midRight, farRight, speedL, speedR );
 			
-			//pc.printf("\n");
-      //pc.printf("pulsesLeft:%d pulsesRight:%d \n", pulsesLeft, pulsesRight);
-    }
+	//pc.printf("\n");
+	pc.printf("pulsesLeft:%d pulsesRight:%d \n", pulsesLeft, pulsesRight);
+  }
 }
