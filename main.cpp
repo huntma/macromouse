657431cd2aaf90098083686f0f3fbff290fe28d1
Hunt Ma <reachhunt@gmail.com>
2017-05-28 11:08:37 -0700

    systick doesn't like wait. change it to ir(). Changing to continuous movement instead of cell by cell. Fix fatal ir() typo of farleft.

--- main.cpp
+++ main.cpp
@@ -28,10 +28,10 @@ DigitalOut eML(PB_12);    //mid left
 DigitalOut eMR(PB_1);    ///mid right
 DigitalOut eR(PB_0);    ///far right
 
-float offsetL;
-float offsetML;
-float offsetMR;
-float offsetR;
+float offsetL=0;
+float offsetML=0;
+float offsetMR=0;
+float offsetR=0;
 
 class cell
 {
@@ -50,7 +50,7 @@ for(i=0; i<mapdim; i++)
 	for(j=0; j<mapdim; j++)
 		map[i][j] = new cell(mapdim + 
 */
-int THRESH = 0.2;
+int THRESH = 0.45;
 int STHRESH = 0.1;
 int MAPSIZE = 16;
 cell map[16][16] = {
@@ -74,10 +74,10 @@ cell map[16][16] = {
 
 int x=0;
 int y=0;
-int curbr=2; //1 for north, 2 for east, 3 south, 4 west
+int curbr=1; //0 for north, 1 for east, 2 south, 3 west
 
 Timer timer;
-Ticker Systicker;
+//Ticker Systicker;
 
 //constants for the code
 double Kp = 0.00015;   // arbitrary values
@@ -85,7 +85,7 @@ double Kd = 0.02;
 double Ki = 0.5;
 
 double maxCorrect = 0.1;   //limits correction values
-double maxSpeed = 0.1;     //limits motor speed
+double maxSpeed = 0.2;     //limits motor speed
 double minSpeed = 0.05;
 
 volatile unsigned long pulsesRight = 0; //why volatile?
@@ -175,91 +175,15 @@ void setRight(double speed){
 	return;
 }
 
-void go(int cells) {
-	int cnt = cells * 146; //TODO change 146
-	bool wall = 0;
-	pulsesRight = 0;
-	pulsesLeft = 0;
-	while(pulsesRight < cnt && pulsesLeft < cnt) {
-		if(midLeft > THRESH || midRight > THRESH) {
-			rightF = 0;
-			leftF = 0;
-			wall = 1;
-			break;
-		}
-		setRight(speedR);
-		setLeft(speedL);
-	}
-	if(wall)
-		placeWall();
-	else	
-		switch(curbr) {
-			case 1:
-				y++;		
-				break;
-			case 2:
-				x++;
-				break;
-			case 3:
-				y--;
-				break;
-			case 4:
-				x--;
-				break;
-		}
-	rightF = 0;
-	leftF = 0;
-	return;
-}
-
-
-void printMap()
+void placeWall(char c) //l for left, r for right, f for front
 {
-for(int i=0; i<MAPSIZE-1; i++)
-	for(int j=0; j<MAPSIZE-1; j++)
-		
-
-
-}
-
-void systick() {
-
-	//--------IR PID----------- 
-	eL = 1;
-	farLeft = rL.read() - offsetL;
-	eL = 0;
-	
-	eML = 1;
-	midLeft = rML.read() - offsetML;
-	eML = 0;
-	
-	eMR = 1;
-	midRight = rMR.read() - offsetMR;
-	eMR = 0;
-
-	eR = 1;
-	farRight = rR.read() - offsetR;
-	eR = 0;
-					
-	if(midRight > STHRESH) { //approaching right wall
-		speedR += 0.2 * midRight;
-		speedL -= 0.2 * midRight;
-		//setRight(speedR);
-		//setLeft(speedL);
-		//pc.printf("speedR : %f\n", speedR);
-	} else if(farLeft > STHRESH) { //may need to make more sensitive
-		speedL += 0.2 * midLeft;
-		speedR -= 0.2 * midLeft;
-		//setLeft(speedL);
-		//setRight(speedR);
-		//pc.printf("speedL: %f\n", speedL);
-	}
-
-}
-
-void placeWall()
-{
-	switch(curbr) { //case 1,2 no danger
+	int dir = curbr;
+	if(c == 'f'){}		//choose front
+	else if(c == 'l') //choose left
+		dir = (dir+3)%4;
+	else							//choose right
+		dir = (dir+1)%4;	
+	switch(dir) { //case 1,2 no danger
 		case 1:
 			map[x][y].n = 1;
 			return;
@@ -275,9 +199,115 @@ void placeWall()
 	}
 }
 
+void rstSpeed()
+{
+	speedR = 0.1;
+	speedL = 0.1;
+}
+
+void ir() {
+	//--------IR PID----------- need 60us to turn on and off
+	eL = 1;
+	wait_us(70);
+	farLeft = rL.read() - offsetL;
+	eL = 0;
+	
+	eMR = 1;
+	wait_us(70);
+	midRight = rMR.read() - offsetMR;
+	eMR = 0;
+
+	eML = 1;
+	wait_us(70);
+	midLeft = rML.read() - offsetML;
+	eML = 0;
+
+	eR = 1;
+	wait_us(70);
+	farRight = rR.read() - offsetR;
+	eR = 0;
+
+	if(midRight > STHRESH) { //approaching right wall
+		speedR += 0.5 * midRight;
+		speedL -= 0.5 * midRight;
+		//pc.printf("speedR : %f\n", speedR);
+	} if(midLeft > STHRESH) { //may need to make more sensitive
+		speedL += 0.5 * midLeft;
+		speedR -= 0.5 * midLeft;
+		//pc.printf("speedL: %f\n", speedL);
+	}
+	return;
+}
+
+void go(int cells) {
+	int cnt = cells * 200; //TODO change
+	bool wall = 0;
+	pulsesRight = 0;
+	pulsesLeft = 0;
+	while(pulsesRight < cnt && pulsesLeft < cnt) {
+		if(midLeft > THRESH || midRight > THRESH) {
+			rightF = 0;
+			leftF = 0;
+			wall = 1;
+			break;
+		}
+		setRight(speedR);
+		setLeft(speedL);
+		rstSpeed();
+	}
+	if(wall)	//if we missed a wall and tried to go into it
+		placeWall('f');
+	else	
+		switch(curbr) {
+			case 1:
+				y++;		
+				break;
+			case 2:
+				x++;
+				break;
+			case 3:
+				y--;
+				break;
+			case 4:
+				x--;
+				break;
+	}
+	rightF = 0;
+	leftF = 0;
+	return;
+}
+
+
+void printMap()
+{
+	int dim = MAPSIZE*2;
+	for(int j=0; j<dim; j++) {
+		for(int i=0; i<dim; i++) {
+			if(j%2 == 0) { //jth row even, print horiz walls
+				if(i%2 == 0) //ith col even, print dots
+					pc.printf(".");
+				else {				 //ith col odd, print north wall if exists
+					if(j<dim && map[i/2][j/2].n) pc.printf("__"); //check bottom
+					else pc.printf(" ");
+				}
+			}
+			else {				//jth row odd, print vert walls
+				if(i%2 == 0) { //ith col even, print walls
+					if(i/2 > 0 && map[i/2-1][j/2].e) pc.printf("|");//check leftest
+					else pc.printf(" ");
+				}
+				else				//ith col odd, print space
+					pc.printf("%d",map[i/2][j/2].dist);
+			}
+		}
+		pc.printf("\n");
+		}
+	return;
+}
+
 int main() {
 	pc.baud(9600);
-	Systicker.attach_us(&systick, 5000);
+	//Systicker.attach_us(&systick, 50000);
 	onled = 1;
 	encoderRightF.rise(&incrementRight);
 	encoderRightF.fall(&incrementRight);
@@ -285,35 +315,41 @@ int main() {
 	encoderLeftF.fall(&incrementLeft);
 	//not use R pins of motors yet
 
-	for(int i=0; i<9; i++) {
+	for(int i=0; i<1; i++) {
 		eL = 1;
-		offsetL  += rL.read();
+		wait_us(100);
+		offsetL = rL.read();
 		eL = 0;
-		eML = 1;
-		offsetML += rML.read();
-		eML = 0;
+		
 		eMR = 1;
-		offsetMR += rMR.read();
+		wait_us(100);
+		offsetMR = rMR.read();
 		eMR = 0;
+
+		eML = 1;
+		wait_us(100);
+		offsetML = rML.read();
+		eML = 0;
+
 		eR = 1;
-		offsetR  += rR.read();
+		wait_us(100);
+		offsetR = rR.read();
 		eR = 0;
 	}
-	offsetL  /= 10;
-	offsetML /= 10;
-	offsetMR /= 10;
-	offsetR  /= 10;
-
-	
 	while (1) {
+	ir();	
+	setLeft(speedL);
+	setRight(speedR);
+	pc.printf("%f %f %f %f %f %f\n", farLeft, midLeft, midRight, farRight, speedL, speedR );
+	rstSpeed();
+/*
 	//check and place walls if possible
 	if(farLeft > THRESH || farRight > THRESH)
 		placeWall();		
-	
+
 	//flood
 
 	//find adjacent lower dist cell
-	
 	int gobr = curbr;
 	int min = map[x][y].dist;
 	if(y > 0 && !map[x][y].n && map[x][y-1].dist < min) //check north
@@ -327,9 +363,8 @@ int main() {
 	turn(gobr);
 	go(1);
 	wait(0.5);
-	
-
-	//	pc.printf("%f %f %f %f %f %f\n", farLeft, midLeft, midRight, farRight, speedL, speedR );
+*/
+//	pc.printf("%f %f %f %f\n", offsetL, offsetML, offsetMR, offsetR);
 			
 	//pc.printf("\n");
 	//pc.printf("pulsesLeft:%d pulsesRight:%d \n", pulsesLeft, pulsesRight);
